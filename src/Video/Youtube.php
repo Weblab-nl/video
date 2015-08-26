0c980076d6025f2d520caf0a3df9c8517612223b
Thomas <thomas@theperfectwedding.nl>
2015-08-26 16:21:48 +0200

    Fixed the naming of the youtube class

--- src/Video/Youtube.php
+++ src/Video/Youtube.php
@@ -3,30 +3,30 @@ namespace Weblab\Video;
 
 /**
  * Helper class to get the youtube video information for a video
- * 
+ *
  * @author Weblab.nl - Thomas Marinissen
  */
-class Youtube extends Tpw_Controller_Helper_Video {
-    
+class Youtube extends \Weblab\Video {
+
     /**
      * Whether the video is low res or not
-     * 
+     *
      * @var boolean
      */
     private $lowRes = false;
 
     /**
      * Constructor
-     * 
+     *
      * @param   string              The video url
      */
     public function __construct($url) {
         parent::__construct($url);
     }
-    
+
     /**
      * Get the video thumbnail
-     * 
+     *
      * @return  string              The video thumbnail url
      */
     public function thumbnail() {
@@ -34,14 +34,14 @@ class Youtube extends Tpw_Controller_Helper_Video {
         if (!is_null($this->thumbnail)) {
             return $this->thumbnail;
         }
-        
+
         // done, add and return the thumbnail
         return $this->thumbnail = '//img.youtube.com/vi/' . $this->id() . '/maxresdefault.jpg';
     }
 
     /**
      * The video embed code
-     * 
+     *
      * @param   int             The video height
      * @param   int             The video width
      * @return  string          The video embed code
@@ -49,39 +49,39 @@ class Youtube extends Tpw_Controller_Helper_Video {
     public function html($height, $width) {
         // set the element that the player element will be linked to
         $html = '<div class="ytplayer" id="' . $this->id() . '" data-width="' . $width . '" data-height="' . $height . '" data-video-id="' . $this->id() . '"></div>';
-        
+
         // return the markup
         return $html;
     }
-    
+
     /**
      * Function to download the thumbnail file
-     * 
+     *
      * @return \Th\FileDownloader
      */
     public function downloadThumb() {
         // call the parent
         $downloader = parent::downloadThumb();
-        
+
         // if there is a downloader, return it
         if (!is_null($downloader)) {
             return $downloader;
         }
-        
+
         // no downloader available, so set a low res thumbnail to make the
         // thumb available still
         $this->thumbnail = '//img.youtube.com/vi/' . $this->id() . '/0.jpg';
-        
+
         // set the video to be low res
         $this->lowRes = true;
-        
+
         // done, return the downloader
         return parent::downloadThumb();
     }
-    
+
     /**
      * Return whether the video is low res or not
-     * 
+     *
      * @return boolean                          Whether the video is low res or not
      */
     public function isLowRes() {
@@ -90,7 +90,7 @@ class Youtube extends Tpw_Controller_Helper_Video {
 
     /**
      * Helper function to parse an video information from the url
-     * 
+     *
      * @param   string                The url to parse
      * @return  \Weblab\Video         The instance of this, to make chaining possible
      */
@@ -99,7 +99,7 @@ class Youtube extends Tpw_Controller_Helper_Video {
         if (preg_match('/youtu\.?be/', $url) == 0) {
             throw new Exception('Not a valid video');
         }
-            
+
         // filter out the id
         if (preg_match('%(?:youtube\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', $url, $match)) {
             $this->id = $match[1];
@@ -111,14 +111,14 @@ class Youtube extends Tpw_Controller_Helper_Video {
         // done, return out
         return $this;
     }
-    
+
     /**
      * Get the videoApiInformation
-     * 
+     *
      * @return mixed                    The video information from the video api of the type
      */
     protected function videoApiInformation() {
         return null;
     }
-    
+
 }
